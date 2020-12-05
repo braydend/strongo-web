69c0f339a3c118cf9e8d9fe21681e446c510b165
Brayden <braydendiluca@gmail.com>
2020-12-06 09:29:10 +1100

    Add basic exercise cards

--- src/components/Pages/ExercisePage.tsx
+++ src/components/Pages/ExercisePage.tsx
@@ -1,4 +1,26 @@
 import React from "react";
+import { Card, Spinner } from "react-bootstrap";
+import { Exercise, useExercises } from "../../hooks/useExercises";
 import Layout from "../Layout";
 
-export const ExercisePage = () => <Layout />;
+const ExerciseCard: React.FC<{ exercise: Exercise }> = ({ exercise }) => {
+  return (
+    <Card>
+      <Card.Header>{exercise.name}</Card.Header>
+    </Card>
+  );
+};
+
+export const ExercisePage = () => {
+  const { data, loading } = useExercises();
+
+  if (loading) return <Spinner animation="border" />;
+
+  return (
+    <Layout>
+      {data?.map((exercise) => (
+        <ExerciseCard exercise={exercise} />
+      ))}
+    </Layout>
+  );
+};
